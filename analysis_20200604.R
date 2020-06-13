62faf6b4732b431ccb3c2425114d8ab58e7f7785
trainorp <patrick.trainor@louisville.edu>
2020-06-12 18:23:53 -0600

    working

--- analysis_20200604.R
+++ analysis_20200604.R
@@ -82,6 +82,19 @@ des1b <- DESeqDataSetFromMatrix(countData = df1b, colData = colData1b, design = 
 des1 <- DESeq(des1)
 des1b <- DESeq(des1b)
 
+# LRT:
+des0 <- DESeq(des1b, test="LRT", reduced = ~ 1)
+res0 <- as.data.frame(results(des0, cooksCutoff = Inf))
+
+# Pre-filter low normalized counts:
+lowNCounts <- rownames(res0)[res0$baseMean < 5]
+des1 <- DESeqDataSetFromMatrix(countData = df1[!rownames(df1) %in% lowNCounts,], 
+                               colData = colData1, design = ~ pheno) # 10726
+des1b <- DESeqDataSetFromMatrix(countData = df1b[!rownames(df1b) %in% lowNCounts,], 
+                                colData = colData1b, design = ~ pheno) # 10714
+des1 <- DESeq(des1)
+des1b <- DESeq(des1b)
+
 # Make data.frame of comparisons between priming and Naive:
 contrastDF1 <- data.frame(pheno1 = c("Enterobacter_Priming", "Serratia_Priming", "Serratia_Priming",
                                      "Enterobacter_infection", "Serratia_Infection",
@@ -102,14 +115,14 @@ for(i in 1:nrow(contrastDF1)){
   contrastDF2[[i]] <- temp1
 }
 contrastDF2 <- do.call("rbind", contrastDF2)
-# save("contrastDF2", "df1b", file = "Results/contrastDF2_20200606.RData")
+
+# Add FDR preserving q-values:
+contrastDF2$qvalue <- qvalue::qvalue(contrastDF2$pvalue)$qvalues
+
+save("contrastDF2", "df1b", file = paste0("Results/contrastDF2_", gsub("-", "", Sys.Date()), ".RData"))
 
 # If p-adjusted is NA make it 1:
 contrastDF2$padj[is.na(contrastDF2$padj)] <- 1.0
-
-# LRT:
-des0 <- DESeq(des1b, test="LRT", reduced = ~ 1)
-res0 <- as.data.frame(results(des0))
 
 # Volcano Plots:
 contrastDF2 <- contrastDF2 %>% mutate(Comparison = case_when(
@@ -126,71 +139,89 @@ contrastDF2$Comparison <- factor(contrastDF2$Comparison, levels =
                                      "Ser Prim & Inf vs Ser Inf"))
 
 # Add labels for volcano plot:
-contrastDF2$volLabel <- ifelse(contrastDF2$padj < 0.05 & abs(contrastDF2$log2FoldChange) > 2.5, 
+contrastDF2$volLabel <- ifelse(contrastDF2$qvalue < 0.05 & abs(contrastDF2$log2FoldChange) > 2.5, 
                                gsub("AgaP_", "", contrastDF2$gene), "")
 
-# png(file = "Plots/PrimingVolcano1.png", height = 4, width = 12, units = "in", res = 600)
+# png(file = paste0("Plots/PrimingVolcano1_", gsub("-","", Sys.Date()),".png"), height = 4, width = 8, units = "in", res = 600)
 set.seed(3333)
-ggplot(contrastDF2 %>% filter(Comparison %in% c("Enterobacter vs. Naive", "Serratia vs. Naive", "Serratia vs. Enterobacter") & padj < 1.0), 
-       aes(x = log2FoldChange, y = -log10(padj), label = volLabel)) + 
+ggplot(contrastDF2 %>% filter(Comparison %in% c("Enterobacter vs. Naive", "Serratia vs. Naive")), 
+       aes(x = log2FoldChange, y = -log10(qvalue), label = volLabel)) + 
   geom_point(shape = 21, color = "grey30", fill = "dodgerblue", alpha = .5, size = .65) + 
   geom_text_repel(size = 1.05, segment.colour = "grey30", segment.alpha = .5, segment.size = .25) +
   geom_vline(xintercept = -2.5, lwd = .25, lty = 2) + geom_vline(xintercept = 2.5, lwd = .25, lty = 2) +
   geom_hline(yintercept = 1.30103, lwd = .25, lty = 2) + 
-  labs(x = expression(paste(log[2], "(Fold Change)")), y = expression(paste(-log[10], "(adjusted p-value)"))) +
+  labs(x = expression(paste(log[2], "(Fold Change)")), y = expression(paste(-log[10], "(q-value)"))) +
   facet_grid(~Comparison)
 # dev.off()
 
+# png(file = paste0("Plots/PrimingVolcano2_", gsub("-","", Sys.Date()),".png"), height = 4, width = 5, units = "in", res = 600)
 set.seed(3333)
+ggplot(contrastDF2 %>% filter(Comparison %in% c("Serratia vs. Enterobacter")), 
+       aes(x = log2FoldChange, y = -log10(qvalue), label = volLabel)) + 
+  geom_point(shape = 21, color = "grey30", fill = "dodgerblue", alpha = .5, size = .65) + 
+  geom_text_repel(size = 2, segment.colour = "grey30", segment.alpha = .5, segment.size = .25) +
+  geom_vline(xintercept = -2.5, lwd = .25, lty = 2) + geom_vline(xintercept = 2.5, lwd = .25, lty = 2) +
+  geom_hline(yintercept = 1.30103, lwd = .25, lty = 2) + 
+  labs(x = expression(paste(log[2], "(Fold Change)")), y = expression(paste(-log[10], "(q-value)")))
+# dev.off()
+
+# png(file = paste0("Plots/InfectionVolcano_", gsub("-","", Sys.Date()),".png"), height = 8, width = 9, units = "in", res = 600)
+set.seed(3333)
+prevLabs <- c("Ent Inf vs Inj Ctrl" = "Enterobacter Infection vs. Inj Ctrl", 
+              "Ser Inf vs Inj Ctrl" = "Serratia Infection vs Inj Ctrl", 
+              "Ent Prim & Inf vs Ent Inf" = "Enterobacter Priming & Infection vs. Infection Only",
+              "Ser Prim & Inf vs Ser Inf" = "Serratia Priming & Infection vs. Infection Only")
 ggplot(contrastDF2 %>% filter(Comparison %in% c("Ent Inf vs Inj Ctrl", "Ser Inf vs Inj Ctrl", "Ent Prim & Inf vs Ent Inf",
-                                                "Ser Prim & Inf vs Ser Inf") & padj < 1.0), 
-       aes(x = log2FoldChange, y = -log10(padj), label = volLabel)) + 
+                                                "Ser Prim & Inf vs Ser Inf")), 
+       aes(x = log2FoldChange, y = -log10(qvalue), label = volLabel)) + 
   geom_point(shape = 21, color = "grey30", fill = "dodgerblue", alpha = .5, size = .65) + 
   geom_text_repel(size = 1.05, segment.colour = "grey30", segment.alpha = .5, segment.size = .25) +
   geom_vline(xintercept = -2.5, lwd = .25, lty = 2) + geom_vline(xintercept = 2.5, lwd = .25, lty = 2) +
   geom_hline(yintercept = 1.30103, lwd = .25, lty = 2) + 
-  labs(x = expression(paste(log[2], "(Fold Change)")), y = expression(paste(-log[10], "(adjusted p-value)"))) +
-  facet_wrap(~Comparison)
+  labs(x = expression(paste(log[2], "(Fold Change)")), y = expression(paste(-log[10], "(q-value)"))) +
+  facet_wrap(~Comparison, labeller = labeller(Comparison = prevLabs))
+rm(prevLabs)
+# dev.off()
 
 # Concordant-discordant analysis:
-contrastDF2wFC <- contrastDF2 %>% select(gene, Comparison, log2FoldChange) %>% spread(key = Comparison, value = log2FoldChange)
-names(contrastDF2wFC)[names(contrastDF2wFC) != "gene"] <- paste0("FC_", names(contrastDF2wFC)[names(contrastDF2wFC) != "gene"])
-contrastDF2wAdjP <- contrastDF2 %>% select(gene, Comparison, padj) %>% spread(key = Comparison, value = padj)
-names(contrastDF2wAdjP)[names(contrastDF2wAdjP) != "gene"] <- 
-  paste0("AdjP_", names(contrastDF2wAdjP)[names(contrastDF2wAdjP) != "gene"])
-contrastDF2w <- contrastDF2wFC %>% left_join(contrastDF2wAdjP)
+contrastDF2wFC <- contrastDF2 %>% select(gene, Comparison, log2FoldChange) %>% 
+  spread(key = Comparison, value = log2FoldChange)
+names(contrastDF2wFC)[names(contrastDF2wFC) != "gene"] <- 
+  paste0("FC_", names(contrastDF2wFC)[names(contrastDF2wFC) != "gene"])
+contrastDF2wQ <- contrastDF2 %>% select(gene, Comparison, qvalue) %>% spread(key = Comparison, value = qvalue)
+names(contrastDF2wQ)[names(contrastDF2wQ) != "gene"] <- 
+  paste0("Q_", names(contrastDF2wQ)[names(contrastDF2wQ) != "gene"])
+contrastDF2w <- contrastDF2wFC %>% left_join(contrastDF2wQ)
 
-cor(x = contrastDF2w$`FC_Ent Inf vs Inj Ctrl`, y = contrastDF2w$`FC_Ser Inf vs Inj Ctrl`)
-ggplot(contrastDF2w, aes(x = `FC_Ent Inf vs Inj Ctrl`, y = `FC_Ser Inf vs Inj Ctrl`)) + 
-  geom_point(color = "dodgerblue", shape = 1, show.legend = FALSE) + stat_smooth(method = "lm")
+ggplot(contrastDF2w %>% filter(`Q_Ent Inf vs Inj Ctrl` < 0.05 | `Q_Ser Inf vs Inj Ctrl` < 0.05), 
+       aes(x = `FC_Ent Inf vs Inj Ctrl`, y = `FC_Ser Inf vs Inj Ctrl`)) + 
+  geom_point(color = "dodgerblue", shape = 1, show.legend = FALSE) + stat_smooth(method = "lm") +
+  xlim(-12, 12) + ylim(-7, 7)
 
 # Priming concordant-discordant analysis:
-contrastDF2a <- contrastDF2 %>% filter(!(pheno2 == "Enterobacter_Priming" & pheno1 ==  "Serratia_Priming"))  %>% select(-pheno2)
-contrastDF2a1 <- contrastDF2a %>% filter(pheno1 == "Enterobacter_Priming")
-contrastDF2a2 <- contrastDF2a %>% filter(pheno1 == "Serratia_Priming")
-contrastDF2a <- contrastDF2a1 %>% full_join(contrastDF2a2 %>% select(-baseMean), by = "gene", suffix = c("_eP", "_sP"))
-contrastDF2a <- contrastDF2a %>% filter(padj_eP < 0.05 | padj_sP < 0.05) # 1,114 genes
+contrastDF2a <- contrastDF2w %>% filter(`Q_Enterobacter vs. Naive` < 0.05 | `Q_Serratia vs. Naive` < 0.05) # 1,162 genes
 # Determine if concordant:
 constrastDF2b <- contrastDF2a %>% mutate(catE = 
-                         case_when(padj_eP < 0.05 & log2FoldChange_eP > 0 ~ "up05",
-                                   padj_eP < 0.05 & log2FoldChange_eP < 0 ~ "down05",
-                                   padj_eP >= 0.05 & log2FoldChange_eP > 0 ~ "upNS",
-                                   padj_eP >= 0.05 & log2FoldChange_eP < 0 ~ "downNS"),
+                         case_when(`Q_Enterobacter vs. Naive` < 0.05 & `FC_Enterobacter vs. Naive` > 0 ~ "up05",
+                                   `Q_Enterobacter vs. Naive` < 0.05 & `FC_Enterobacter vs. Naive` < 0 ~ "down05",
+                                   `Q_Enterobacter vs. Naive` >= 0.05 & `FC_Enterobacter vs. Naive` > 0 ~ "upNS",
+                                   `Q_Enterobacter vs. Naive` >= 0.05 & `FC_Enterobacter vs. Naive` < 0 ~ "downNS"),
                          catS = 
-                           case_when(padj_sP < 0.05 & log2FoldChange_sP > 0 ~ "up05",
-                                     padj_sP < 0.05 & log2FoldChange_sP < 0 ~ "down05",
-                                     padj_sP >= 0.05 & log2FoldChange_sP > 0 ~ "upNS",
-                                     padj_sP >= 0.05 & log2FoldChange_sP < 0 ~ "downNS"))
+                           case_when(`Q_Serratia vs. Naive` < 0.05 & `FC_Serratia vs. Naive` > 0 ~ "up05",
+                                     `Q_Serratia vs. Naive` < 0.05 & `FC_Serratia vs. Naive` < 0 ~ "down05",
+                                     `Q_Serratia vs. Naive` >= 0.05 & `FC_Serratia vs. Naive`> 0 ~ "upNS",
+                                     `Q_Serratia vs. Naive` >= 0.05 & `FC_Serratia vs. Naive` < 0 ~ "downNS"))
 xtabs(~catE + catS, data = constrastDF2b)
 
 # Label some:
 contrastDF2a$lab <- contrastDF2a$gene
 contrastDF2a$lab <- gsub("AgaP_", "", contrastDF2a$lab)
-contrastDF2a$lab[!(abs(contrastDF2a$log2FoldChange_eP) > 2.5 | abs(contrastDF2a$log2FoldChange_sP) > 2.5)] <- ""
+contrastDF2a$lab[!(abs(contrastDF2a$`FC_Enterobacter vs. Naive`) > 2.5 | 
+                     abs(contrastDF2a$`FC_Serratia vs. Naive`) > 2.5)] <- ""
 
-# png(file = "Plots/PrimingAgreement1.png", height = 5, width = 6.25, units = "in", res = 300)
+# png(file = "Plots/PrimingAgreement1.png", height = 5, width = 6, units = "in", res = 300)
 set.seed(3)
-ggplot(contrastDF2a, aes(x = log2FoldChange_eP, y = log2FoldChange_sP, label = lab)) + 
+ggplot(contrastDF2a, aes(x = `FC_Enterobacter vs. Naive`, y = `FC_Serratia vs. Naive`, label = lab)) + 
   geom_point(color = "dodgerblue", shape = 1, show.legend = FALSE) + 
   geom_text_repel(size = 2, segment.colour = "grey30", segment.alpha = .5) +
   geom_vline(xintercept = 0, lty = 2, lwd = .25) + geom_hline(yintercept = 0, lty = 2, lwd = .25) +
@@ -198,12 +229,12 @@ ggplot(contrastDF2a, aes(x = log2FoldChange_eP, y = log2FoldChange_sP, label = l
 # dev.off()
 
 # Comparison of priming types:
-contrastDF2c <- contrastDF2 %>% filter(pheno1 == "Enterobacter_Priming" & pheno2 == "Serratia_Priming" & padj < .05)
+contrastDF2c <- contrastDF2 %>% filter(pheno1 == "Enterobacter_Priming" & pheno2 == "Serratia_Priming" & qvalue < .05)
 
-rm(constrastDF2b, contrastDF1, contrastDF2a, contrastDF2a1, contrastDF2a2, contrastDF2c, des0,
-   res0, temp1, i, filter1, rmExtra1)
+rm(constrastDF2b, contrastDF1, contrastDF2a, contrastDF2w, contrastDF2wQ, contrastDF2wFC, lowNCounts, des0,
+   res0, temp1, i, filter1, rmExtra1, filter1b)
 
-############ Transformation & Entropy filtering ############
+############ Transformation & Entropy / Significance filtering ############
 # Get regularized log expression:
 rlog1 <- rlog(des1, blind = TRUE)
 rlog1b <- rlog(des1b, blind = TRUE)
@@ -225,8 +256,8 @@ rlog1Tempb$oldSampName <- rownames(rlog1Tempb)
 rlog1Temp <- colData1 %>% select(oldSampName, pheno) %>% left_join(rlog1Temp) %>% select(-oldSampName)
 rlog1Tempb <- colData1b %>% select(oldSampName, pheno) %>% left_join(rlog1Tempb) %>% select(-oldSampName)
 
-rlog1Entropy <- FSelectorRcpp::information_gain(pheno ~ ., data = rlog1Temp)
-rlog1Entropyb <- FSelectorRcpp::information_gain(pheno ~ ., data = rlog1Tempb)
+rlog1Entropy <- FSelectorRcpp::information_gain(pheno ~ ., data = rlog1Temp, type = "gainratio")
+rlog1Entropyb <- FSelectorRcpp::information_gain(pheno ~ ., data = rlog1Tempb, type = "gainratio")
 
 rlog1Entropy <- rlog1Entropy %>% select(gene = attributes, infoGain = importance)
 rlog1Entropyb <- rlog1Entropyb %>% select(gene = attributes, infoGain = importance)
@@ -248,7 +279,7 @@ median(rlog1Entropy$entropy)
 median(rlog1Entropy$infoGain)
 
 ############ Multivariate visualization ############
-pca1 <- prcomp(scale(rlog2, scale = FALSE))
+pca1 <- prcomp(scale(rlog1, scale = FALSE))
 pca1Scores <- as.data.frame(pca1$x[,1:4])
 pca1Scores$oldSampName <- rownames(pca1Scores)
 pca1Scores <- colData1 %>% left_join(pca1Scores)
@@ -273,11 +304,11 @@ PCA3D <- PCA3D %>% plotly::layout(scene = list(xaxis = list(title = 'PC1'),
 PCA3D
 
 png(filename = "Plots/hclust.png", height = 6.5, width = 6.5, units = "in", res = 300)
-plot(hclust(dist(rlog2), method = "ward.D2"))
+plot(hclust(dist(rlog1), method = "ward.D2"))
 dev.off()
 
 ############ Sensitivity analysis ############
-pca2 <- prcomp(scale(rlog2[rownames(rlog2) != "Ent Prim + Ser inf 2",], scale = FALSE))
+pca2 <- prcomp(scale(rlog1[rownames(rlog1) != "Ent Prim + Ser inf 2",], scale = FALSE))
 pca2Scores <- as.data.frame(pca2$x[,1:4])
 pca2Scores$oldSampName <- rownames(pca2Scores)
 pca2Scores <- colData1 %>% inner_join(pca2Scores)
@@ -295,18 +326,15 @@ ggplot(pca2Scores, aes(x = PC3, y = PC4, color = pheno, label = oldSampName)) + 
 dev.off()
 
 png(filename = "Plots/hclust_sens.png", height = 6.5, width = 6.5, units = "in", res = 300)
-plot(hclust(dist(rlog2[rownames(rlog2) != "Ent Prim + Ser inf 2",]), method = "ward.D2"))
+plot(hclust(dist(rlog1[rownames(rlog1) != "Ent Prim + Ser inf 2",]), method = "ward.D2"))
 dev.off()
 
 rm(pca1, pca2, pca1Scores, pca2Scores, PCA3D, colEntropies, colEntropiesb)
 
 ############ WGCNA "fitting" ############
-
 # Entropy filters on both:
-rlog2 <- rlog1[, (rlog1Entropy$entropy > quantile(rlog1Entropy$entropy)['75%'] | 
-                    rlog1Entropy$infoGain > quantile(rlog1Entropy$infoGain)['75%'])] # 4,973
-rlog2b <- rlog1b[, (rlog1Entropyb$entropy > quantile(rlog1Entropyb$entropy)['75%'] | 
-                      rlog1Entropyb$infoGain > quantile(rlog1Entropyb$infoGain)['75%'])] # 4,973
+rlog2b <- rlog1b[, (rlog1Entropyb$entropy > quantile(rlog1Entropyb$entropy)['50%'])] # 4,973
+rlog2b <- rlog1b
 
 powers <- 1:20
 sft <- WGCNA::pickSoftThreshold(rlog2b, powerVector = powers, verbose = 5)
@@ -318,15 +346,15 @@ plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
 text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
      labels = powers, col = "red")
 # this line corresponds to using an R^2 cut-off of h
-abline(h = 0.80, col = "red")
+abline(h = 0.95, col = "red")
 plot(sft$fitIndices[,1], sft$fitIndices[,5], xlab = "Soft Threshold (power)", ylab = "Mean Connectivity", type = "n",
      main = paste("Mean connectivity"))
 text(sft$fitIndices[,1], sft$fitIndices[,5], labels = powers, col = "red")
-softPower <- 6
+softPower <- 9
 par(oldPar)
 
 # Adjacency and distance matrices
-adjacency <- WGCNA::adjacency(rlog2b, power = softPower) #corOptions="use = 'p', method = 'spearman'"
+adjacency <- WGCNA::adjacency(rlog2b, type = "signed", power = softPower) #corOptions="use = 'p', method = 'spearman'"
 # Turn adjacency into topological overlap
 TOM <- WGCNA::TOMsimilarity(adjacency)
 rownames(TOM) <- colnames(TOM) <- rownames(adjacency)
